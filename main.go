2e0d0d11fcd7c74b132f86c59208e63fa6b06deb
youngstone89 <youngstone89@icloud.com>
2022-01-30 20:52:47 +0900

    merged

--- main.go
+++ main.go
@@ -1,10 +1,5 @@
 package main
 
-import (
-	"go-mastery/pkg/crypto"
-	"go-mastery/pkg/timer"
-)
-
 func main() {
 
 	//confluentkafkago.DoProduce()
@@ -22,27 +17,9 @@ func main() {
 	//ticker.DoTickerTest(&wg)
 	//wg.Wait()
 	//minio.DoMinioTest()
-<<<<<<< HEAD
-	crypto.DoCryptoTest()
-}
-=======
-	// crypto.DoCryptoTest()
-	// start := time.Now()
-	// timer.DoTest2(1)
-	// fmt.Println(time.Since(start))
-	// confluentkafkago.DoProduce()
-	// pipeline.Run()
-	// confluentkafkago.StartConfluentKafkaConsumerProgram()
-	// selecttest.Dotest()
-
-	// semaphores.DoBasic()
-	// semaphores.DoIntermediate()
-	// waitgroups.DoBasic()
->>>>>>> development
 
 	// iterators.DoCallBackLoop(5)
 	// queue.DoJob(10000, 10)
-	timer.DoTest(1)
 	// timer.DoTest2(1)
 
 }
