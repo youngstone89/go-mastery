94a3c2de452f4c5eae3c430660fb087c94a50eb4
YeongSeok Kim <youngstone89@icloud.com>
2022-05-13 12:21:00 +0900

    added sword test

--- pkg/software-engineering-in-go/openandclose/sword_test.go
+++ pkg/software-engineering-in-go/openandclose/sword_test.go
@@ -32,6 +32,14 @@ func (EnchantedSword) Damage() int {
 	return 42
 }
 
+// String implements fmt.Stringer for the Sword type.
+func (s EnchantedSword) String() string {
+	return fmt.Sprintf(
+		"%s is a sword that can deal %d points of damage to opponents",
+		s.name, s.Damage(),
+	)
+}
+
 func TestEnchanctedSword(t *testing.T) {
 	specs := []struct {
 		sword interface {
@@ -51,6 +59,28 @@ func TestEnchanctedSword(t *testing.T) {
 	for specIndex, spec := range specs {
 		if got := spec.sword.Damage(); got != spec.exp {
 			t.Errorf("[spec %d] expected to get damage %d; got %d", specIndex, spec.exp, got)
+		}
+	}
+}
+
+func TestSwordToString(t *testing.T) {
+	specs := []struct {
+		sword fmt.Stringer
+		exp   string
+	}{
+		{
+			sword: Sword{name: "Silver Saber"},
+			exp:   "Silver Saber is a sword that can deal 2 points of damage to opponents",
+		},
+		{
+			sword: EnchantedSword{Sword{name: "Dragon's Greatsword"}},
+			exp:   "Dragon's Greatsword is a sword that can deal 42 points of damage to opponents",
+		},
+	}
+	for specIndex, spec := range specs {
+		if got := spec.sword.String(); got != spec.exp {
+			t.Errorf("[spec %d] expected to get\n%q\ngot:\n%q",
+				specIndex, spec.exp, got)
 		}
 	}
 }
